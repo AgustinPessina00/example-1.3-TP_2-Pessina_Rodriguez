c8493afd0f509899902cf18e749de2df54290e8c
AgustinPessina00 <apessina@fi.uba.ar>
2023-09-02 21:46:58 +0000

    Eliminamos las clases y usamos estructuras. Agregamos printf.
    Co-authored-by: ezerod <ezrodriguez@fi.uba.ar>

--- main.cpp
+++ main.cpp
@@ -1,31 +1,38 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+//=====[Biblioteca necesaria para usar la estructura gpio_t]========
+#include "hal/gpio_api.h"
+
 int main()
 {
-    DigitalIn gasDetector(D2);
-    DigitalIn overTempDetector(D3);
-    DigitalIn alarmOffButton(BUTTON1);
-
-    DigitalOut alarmLed(LED1);
-
-    gasDetector.mode(PullDown);
-    overTempDetector.mode(PullDown);
-
-    alarmLed = OFF;
+    gpio_t gpioGasDetector;
+    gpio_t gpioOverTempDetector;
+    gpio_t gpioAlarmOffButton;
+    gpio_t gpioAlarmLed;
+    gpio_init_in_ex(&gpioGasDetector, D2, PullDown);
+    gpio_init_in_ex(&gpioOverTempDetector, D2, PullDown);
+    gpio_init_in(&gpioAlarmOffButton, BUTTON1);
+    gpio_init_out(&gpioAlarmLed, LED1);
+    
+    gpio_write(&gpioAlarmLed, OFF);
 
     bool alarmState = OFF;
 
     while (true) {
 
-        if ( gasDetector || overTempDetector ) {
+        if ( gpio_read(&gpioGasDetector) || gpio_read(&gpioOverTempDetector) ) {
             alarmState = ON;
+            printf("%s%s\n", "El sensor de gas está ", gpio_read(&gpioGasDetector) ? "ACTIVADO" : "DESACTIVADO");
+            printf("%s%s\n", "El sensor de sobretemperatura está ", gpio_read(&gpioOverTempDetector) ? "ACTIVADO" : "DESACTIVADO");
+            printf("%s\n" , "El estado de la alarma es ACTIVADO.");
         }
 
-        alarmLed = alarmState;
+        gpio_write(&gpioAlarmLed, alarmState);
 
-        if ( alarmOffButton ) {
+        if ( gpio_read(&gpioAlarmOffButton) ) {
             alarmState = OFF;
+            printf("%s\n" , "El estado de la alarma es DESACTIVADO.");
         }
     }
 }
